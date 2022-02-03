6f609159f1d671c60a3ff8e6b8a3e81c3d560370
Foad Alhsnat <57833439+FoadAlhsnat@users.noreply.github.com>
2022-02-03 20:46:20 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,42 +1,11 @@
-// import express from 'express'
-// import dotenv from 'dotenv'
-// // import connectDB from './config/db.js'
-// // import productRoute from './routes/product.route.js'
-// // import userRoute from './routes/userRout.js'
-// // import cors from 'cors'
-// dotenv.config()
-
-
-// // connectDB()
-// const app = express()
-// app.use(express.json)
-// const PORT = process.env.PORT || 5000;
-// // app.use(cors())
-
-// // app.use('/api/users', userRoute)
-// // app.use('/api/products', productRoute)
-
-
-
-
-
-// app.get('/', (req, res) => {
-//   console.log('hi');
-//   res.send("api is running")
-// })
-
-
-
-
-
-
-
-
 import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoute from './routes/product.route.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import userRoute from './routes/userRout.js'
 import orderROute from './routes/orderROute.js'
 import cors from 'cors'
@@ -54,7 +23,15 @@ app.use((bodyParser.json()))
 app.use('/api/orders', orderROute)
 app.use('/api/products', productRoute)
 app.use('/api/users', userRoute)
+app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLINT_ID))
+
+app.use(notFound)
+app.use(errorHandler)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
