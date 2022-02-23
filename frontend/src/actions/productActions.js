62d36d287a9651b46a386fa409790d7035806cec
Foad Alhsnat <57833439+FoadAlhsnat@users.noreply.github.com>
2022-02-23 19:09:09 +0200

    Update productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -18,9 +18,6 @@ import {
   PRODUCT_CREATE_REVIEW_REQUEST,
   PRODUCT_CREATE_REVIEW_SUCCESS,
   PRODUCT_CREATE_REVIEW_FAIL,
-  PRODUCT_TOP_REQUEST,
-  PRODUCT_TOP_SUCCESS,
-  PRODUCT_TOP_FAIL,
 } from '../constants/productConstants'
 import { logout } from './userActions'
 
@@ -226,23 +223,3 @@ export const createProductReview = (productId, review) => async (
   }
 }
 
-export const listTopProducts = () => async (dispatch) => {
-  try {
-    dispatch({ type: PRODUCT_TOP_REQUEST })
-
-    const { data } = await axios.get(`/api/products/top`)
-
-    dispatch({
-      type: PRODUCT_TOP_SUCCESS,
-      payload: data,
-    })
-  } catch (error) {
-    dispatch({
-      type: PRODUCT_TOP_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
