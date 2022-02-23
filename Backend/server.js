f4617d9c6c73e3370b4b71ffade754af1ee691f4
Foad Alhsnat <57833439+FoadAlhsnat@users.noreply.github.com>
2022-02-23 19:17:08 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
@@ -29,22 +28,10 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
+app.get('/api/config/paypal', (req, res) =>{
+   res.send(process.env.PAYPAL_CLINT_ID)
+}
 )
-=======
-import express from 'express'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import productRoute from './routes/product.route.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import uploadRoutes from './routes/uploadRoutes.js'
-import userRoute from './routes/userRout.js'
-import orderROute from './routes/orderROute.js'
-import cors from 'cors'
->>>>>>> da51bf97229775098cd9bac21150994543060080
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
@@ -64,31 +51,11 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-<<<<<<< HEAD
 const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 )
-=======
-app.use('/api/orders', orderROute)
-app.use('/api/products', productRoute)
-app.use('/api/users', userRoute)
-app.use('/api/upload', uploadRoutes)
-app.get('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLINT_ID))
-
-app.use(notFound)
-app.use(errorHandler)
-
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(PORT, () => console.log(`server run in ${process.env.NODE_ENV} on port ${PORT}`))
->>>>>>> da51bf97229775098cd9bac21150994543060080
