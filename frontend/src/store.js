1ceb9aca5e543fca6c5bedf2a907eca5ca3667a9
Foad Alhsnat <57833439+FoadAlhsnat@users.noreply.github.com>
2022-02-23 19:10:47 +0200

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,7 +8,6 @@ import {
   productCreateReducer,
   productUpdateReducer,
   productReviewCreateReducer,
-  productTopRatedReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
@@ -36,7 +35,6 @@ const reducer = combineReducers({
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
   productReviewCreate: productReviewCreateReducer,
-  productTopRated: productTopRatedReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
